394b2a7cabd18d1b15a5efeee936d85aad71fc56
Kursat Keskin <kursatkeskinn@gmail.com>
2023-09-30 02:26:23 +0100

    admin panel ui changed, footer added

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -46,12 +46,10 @@ const Login = () => {
     const input_style =
     "form-control block w-full px-4 py-5 text-sm font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none";
 
-
     return (
-
-              <section className="bg-ct-blue-600 mt-10">
-        <div className="mx-auto h-full flex justify-center items-center">
-          <div className="bg-white py-10"></div>
+              <section className="bg-ct-blue-600 mt-10 h-screen">
+        <div className="mx-auto py-5 flex justify-center items-center">
+          <div className="bg-white"></div>
         <div className="grid lg:grid-cols-2 sm:grid-cols-1 max-w-6xl">
     <div className="flex justify-center items-center">
       <Image
